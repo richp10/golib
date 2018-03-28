764e0639ff7d644719b20b4aa6b64c4e821d8681
Richard Phillips <richp10@gmail.com>
2018-03-28 16:56:51 +0100

    ConsulwatchIP uses key instead of node name

--- env/consulwatch.go
+++ env/consulwatch.go
@@ -7,8 +7,10 @@ import (
 	"github.com/spf13/viper"
 
 	"github.com/armon/consul-api"
+	"github.com/asaskevich/govalidator"
 )
 
+// Watches a Consul *Node*
 func ConsulWatch(nodename string, key string, schema string) {
 	// Get a new consul client
 	if key == "" {
@@ -50,5 +52,50 @@ func ConsulWatch(nodename string, key string, schema string) {
 	} else {
 		log.Fatalf("No docker container: " + nodename)
 	}
+
+}
+
+// Watches using a consul KV Key
+func ConsulWatchIpKey(nodename string, key string, schema string) {
+	// Get a new consul client
+	if key == "" {
+		key = nodename
+	}
+
+	consul, err := consulapi.NewClient(consulapi.DefaultConfig())
+	if err != nil {
+		log.Fatalf("err: %v", err)
+	}
+
+	// open a goroutine to watch remote changes forever
+	go func() {
+		ip := ""
+		for {
+			kvpair, _, err := consul.KV().Get("ip-store/"+nodename, nil)
+			if err != nil {
+				log.Info("Consul Get KV Failed: ", err)
+				time.Sleep(time.Second * 10) // delay after fail..
+				continue
+			}
+
+			if !govalidator.IsIP(string(kvpair.Value)) {
+				log.Info("Consul Get KV IP Invalid")
+				time.Sleep(time.Second * 10) // delay after fail..
+				continue
+			}
+
+			newip := string(kvpair.Value)
+			if ip != newip {
+				// First set the requested environment setting using the ip and optional schema
+				log.Info("SET KEY: " + key + " TO: " + schema + newip)
+				viper.Set(key, schema+newip)
+				ip = newip
+				// Create a flag that we can use to check whether
+				// the node IP address has been changed
+				viper.Set(nodename+"-ipchanged", true)
+			}
+			time.Sleep(time.Second * 10) // delay after each request
+		}
+	}()
 
 }
